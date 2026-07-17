e251c973a5db2d536a433ad813fe865a735328b4
Alok1720 <alokm172005@gmail.com>
2026-07-17 20:49:34 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (92.00%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        do{
+            if(ans.empty() || ans.back()!=nums){
+                ans.push_back(nums);
+            
+            }
+        }while(next_permutation(nums.begin(), nums.end()));
+
+        return ans;
+        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
