134934393f1b8c445fd668bb1f0c62d4ed6c547c
Alok1720 <alokm172005@gmail.com>
2026-08-06 22:00:34 +0530

    Time: 4 ms (44.11%) | Memory: 147.1 MB (6.46%) - LeetSync

---
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+
+        // If left child doesn't exist
+        if (root->left == nullptr)
+            return 1 + minDepth(root->right);
+
+        // If right child doesn't exist
+        if (root->right == nullptr)
+            return 1 + minDepth(root->left);
+
+        // Both children exist
+        return 1 + min(minDepth(root->left), minDepth(root->right));
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
