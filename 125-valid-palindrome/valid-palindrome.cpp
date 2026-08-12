536930ad1cdcf30fdecccd4b796ddd0bdfb2f2dd
Alok1720 <alokm172005@gmail.com>
2026-08-12 21:51:53 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (54.13%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            // Skip non-alphanumeric characters
+            while (left < right && !isalnum(s[left])) {
+                left++;
+            }
+
+            while (left < right && !isalnum(s[right])) {
+                right--;
+            }
+
+            // Compare characters ignoring case
+            if (tolower(s[left]) != tolower(s[right])) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
