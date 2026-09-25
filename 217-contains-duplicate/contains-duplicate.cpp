d13cf23f995b5294391a2a4f23db79346ddea930
Alok1720 <alokm172005@gmail.com>
2026-09-25 10:55:57 +0530

    Time: 64 ms (73.59%) | Memory: 111.2 MB (74.03%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+
+        for (int num : nums) {
+            if (seen.find(num) != seen.end()) {
+                return true;
+            }
+
+            seen.insert(num);
+        }
+
+        return false;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
